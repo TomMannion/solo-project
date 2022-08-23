88d323a0be725e8f707810b62be89ac2bc5ead95
postie89 <t.mannion89@gmail.com>
2022-08-23 11:48:56 +0100

    removed redundant remove_from_order method in customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,9 +19,6 @@ class Customer
       @order << menu_item
     end
   end
-  def remove_from_order(menu_item)
-    @order.delete(menu_item)
-  end
   def total_order_price
     @order.reduce(0) { |sum, menu_item| sum + menu_item.price.to_f }
   end
